e7e6179ccaf803a08494068be271d45ecd805849
jerrylin1020 <hungwen.lin@gmail.com>
2014-01-23 17:23:57 +0800

    update files for firxfox proxy issue

--- MyAccount.bat
+++ MyAccount.bat
@@ -1,1 +1,1 @@
-pybot --variable phase:beta -o C:\CI\MyAccount\MyAccountRobot\output.xml -l C:\CI\MyAccount\MyAccountRobot\log.html -r C:\CI\MyAccount\MyAccountRobot\report.html C:\CI\MyAccount\MyAccountRobot\TestSuite
+pybot --variable phase:beta -o C:\CI\MyAccount\MyAccountRobot\output.xml -l C:\CI\MyAccount\MyAccountRobot\log.html -r C:\CI\MyAccount\MyAccountRobot\report.html TestSuite
